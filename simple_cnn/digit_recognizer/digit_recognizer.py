d67d29d543f00a618461b9bb106b1a0bfc43826a
hzzz <hzzz0823@gmail.com>
2020-04-16 21:25:15 +0800

    LearningRateScheduler
    看不出效果，主要当前0.992也没什么提升空间了。

--- simple_cnn/digit_recognizer/digit_recognizer.py
+++ simple_cnn/digit_recognizer/digit_recognizer.py
@@ -4,6 +4,16 @@ from tensorflow.keras.layers import Dense, Conv2D, AveragePooling2D, Activation,
 from tensorflow.keras.utils import to_categorical
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.callbacks import LearningRateScheduler, EarlyStopping
+import math
+
+def decay(epoch):
+    if epoch < 10:
+        return 0.01
+    elif epoch < 50:
+        return 0.001
+    else:
+        return 0.01 / epoch
 
 
 def graph(history):
@@ -40,18 +50,18 @@ Y = to_categorical(Y, 10)
 X_train = df_train.values.reshape((-1, 28, 28, 1)) / 255.
 # X_test = df_test.values.reshape((-1, 28, 28, 1)) / 255.
 
-X_train, X_test, y_train, y_test = train_test_split(X_train, Y, test_size=0.1, random_state=2)
-
 model = keras.models.Sequential()
-model.add(Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)))
+model.add(Conv2D(32, 3, input_shape=(28, 28, 1)))
+model.add(Conv2D(32, 3))
+model.add(MaxPooling2D())
+model.add(Dropout(0.5))
 model.add(Conv2D(64, 3, activation='relu'))
 model.add(MaxPooling2D())
-model.add(Dropout(0.2))
+model.add(Dropout(0.5))
+model.add(Conv2D(120, 5, activation='relu'))
 model.add(Flatten())
-model.add(Dense(units=128, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(units=10, activation='softmax'))
-
+model.add(Dense(84, activation='relu'))
+model.add(Dense(10, activation='softmax'))
 model.summary()
 
 optimizer = keras.optimizers.Adam(learning_rate=0.001)
@@ -59,10 +69,12 @@ model.compile(loss='categorical_crossentropy',
               optimizer=optimizer,
               metrics=['accuracy'])
 
-history = model.fit(X_train, y_train,
-                    batch_size=64,
-                    epochs=100,
-                    validation_data=(X_test, y_test))
+earlyStopping = EarlyStopping(monitor='val_loss', patience=30, verbose=1, restore_best_weights=True)
+history = model.fit(X_train, Y,
+                    batch_size=2560,
+                    epochs=300,
+                    callbacks=[LearningRateScheduler(decay), earlyStopping],
+                    validation_split=0.1)
 
 model.save('simple_cnn.h5')
 print('model save')
